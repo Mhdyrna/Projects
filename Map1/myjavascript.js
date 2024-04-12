21b792a521723abf61a518c66e23450099d15f16
mhdyrna <mahdiyarnahavandi@gmail.com>
2024-04-12 23:15:03 +0430

    Upload simple PWA project on Github

---
+++ Map1/myjavascript.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+
+    $('#IRN3209').mouseover(function(){
+      $('.showcityname').html('کهگیلویه');
+      $('.showcityname').append("<img src='Kohgiluyeh.jpg'>");
+    });
+    $('#IRN3248').mouseover(function(){
+      $('.showcityname').html('کرمان');
+      $('.showcityname').append("<img src='Kerman.jpg' width='200' height='200'>");
+    });
+    $('#IRN3210').mouseover(function(){
+      $('.showcityname').html('بوشهر');
+      $('.showcityname').append("<img src='Bushehr.jpg' width='200' height='200'>");
+    });
+    $('#IRN3211').mouseover(function(){
+      $('.showcityname').html('اصفهان');
+      $('.showcityname').append("<img src='esfahan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3212').mouseover(function(){
+      $('.showcityname').html('شیراز');
+      $('.showcityname').append("<img src='shiraz.jpg' width='200' height='200'>");
+    });
+    $('#IRN3213').mouseover(function(){
+      $('.showcityname').html('گلستان');
+      $('.showcityname').append("<img src='golestan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3215').mouseover(function(){
+      $('.showcityname').html('سمنان');
+      $('.showcityname').append("<img src='semnan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3216').mouseover(function(){
+      $('.showcityname').html('تهران');
+      $('.showcityname').append("<img src='tehran.jpg' width='200' height='200'>");
+    });
+    $('#IRN3217').mouseover(function(){
+      $('.showcityname').html('یزد');
+      $('.showcityname').append("<img src='yazd.jpg' width='200' height='200'>");
+    });
+    $('#IRN3218').mouseover(function(){
+      $('.showcityname').html('چهار محال بختیاری');
+      $('.showcityname').append("<img src='chaharmahal.jpg' width='200' height='200'>");
+    });
+    $('#IRN3219').mouseover(function(){
+      $('.showcityname').html('اهواز');
+      $('.showcityname').append("<img src='khoozestan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3220').mouseover(function(){
+      $('.showcityname').html('لرستان');
+      $('.showcityname').append("<img src='lorestan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3221').mouseover(function(){
+      $('.showcityname').html('ایلام');
+      $('.showcityname').append("<img src='ilam.jpg' width='200' height='200'>");
+    });
+    $('#IRN3228').mouseover(function(){
+      $('.showcityname').html('هرمزگان');
+      $('.showcityname').append("<img src='hormozgan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3230').mouseover(function(){
+      $('.showcityname').html('اردبیل');
+      $('.showcityname').append("<img src='ardebil.jpg' width='200' height='200'>");
+    });
+    $('#IRN3231').mouseover(function(){
+      $('.showcityname').html('اراک');
+      $('.showcityname').append("<img src='arak.jpg' width='200' height='200'>");
+    });
+    $('#IRN3232').mouseover(function(){
+      $('.showcityname').html('قم');
+      $('.showcityname').append("<img src='qom.jpg' width='200' height='200'>");
+    });
+    $('#IRN3233').mouseover(function(){
+      $('.showcityname').html('همدان');
+      $('.showcityname').append("<img src='hamedan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3235').mouseover(function(){
+      $('.showcityname').html('قزوین');
+      $('.showcityname').append("<img src='qazvin.jpg' width='200' height='200'>");
+    });
+    $('#IRN3237').mouseover(function(){
+      $('.showcityname').html('آذربایجان غربی');
+      $('.showcityname').append("<img src='wazarbaijan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3238').mouseover(function(){
+      $('.showcityname').html('آذربایجان شرقی');
+      $('.showcityname').append("<img src='eazarbaijan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3239').mouseover(function(){
+      $('.showcityname').html('کرمانشاه');
+      $('.showcityname').append("<img src='kermanshah.jpg' width='200' height='200'>");
+    });
+    $('#IRN3240').mouseover(function(){
+      $('.showcityname').html('رشت');
+      $('.showcityname').append("<img src='rasht.jpg' width='200' height='200'>");
+    });
+    $('#IRN3241').mouseover(function(){
+      $('.showcityname').html('کردستان');
+      $('.showcityname').append("<img src='kordestan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3244').mouseover(function(){
+      $('.showcityname').html('خراسان جنوبی');
+      $('.showcityname').append("<img src='khjonoobi.jpg' width='200' height='200'>");
+    });
+    $('#IRN3245').mouseover(function(){
+      $('.showcityname').html('مشهد');
+      $('.showcityname').append("<img src='mashhad.jpg' width='200' height='200'>");
+    });
+    $('#IRN3246').mouseover(function(){
+      $('.showcityname').html('خراسان شمالی');
+      $('.showcityname').append("<img src='khshomali.jpg' width='200' height='200'>");
+    });
+    $('#IRN3247').mouseover(function(){
+      $('.showcityname').html('سیستان و بلوچستان');
+      $('.showcityname').append("<img src='baloochestan.jpg' width='200' height='200'>");
+    });
+    $('#IRN5490').mouseover(function(){
+      $('.showcityname').html('البرز');
+      $('.showcityname').append("<img src='alborz.jpg' width='200' height='200'>");
+    });
+    $('#IRN3234').mouseover(function(){
+      $('.showcityname').html('زنجان');
+      $('.showcityname').append("<img src='zanjan.jpg' width='200' height='200'>");
+    });
+    $('#IRN3214').mouseover(function(){
+      $('.showcityname').html('ساری');
+      $('.showcityname').append("<img src='mazandaran.jpg' width='200' height='200'>");
+    });
+
+
+
+
+
+
+
+});
